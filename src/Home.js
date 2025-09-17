b7b42ab7bb10aa9c42feebf681002c830d9be21b
DigitallLearning <digitallearning1505@gmail.com>
2025-09-17 13:02:29 +0530

    This is my First ecoomerce app

---
+++ src/Home.js
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react"
+import React from 'react';
+import {
+  MDBCard,
+  MDBCardImage,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBRow,
+  MDBCol,
+  MDBBtn
+} from 'mdb-react-ui-kit';
+function Home()
+{
+    const [apidata,setData]=useState([])
+    useEffect(()=>{
+       fetch("https://fakestoreapi.com/products").then((result)=>{
+           result.json().then((data)=>{
+                setData(data)
+           })
+       })
+    },[])
+  return(
+    <div>
+       <MDBRow className='row-cols-1 row-cols-md-3 g-4' style={{padding:"10px"}}>
+       {
+        apidata.map((item)=>
+       <MDBCol >
+        <MDBCard className='h-100' style={{padding:"10px"}}>
+         <center>
+             <MDBCardImage
+            src={item.image}
+            alt='...'
+            position='top'
+            style={{width:"100px",height:"100px"}}
+          />
+         </center>
+          <MDBCardBody>
+            <MDBCardTitle style={{color:"red"}}>{item.title}</MDBCardTitle>
+             <MDBCardTitle style={{color:"green"}}>{item.price*85} Rs</MDBCardTitle>
+              <MDBCardTitle style={{color:"blue"}}>{item.category}</MDBCardTitle>
+            <MDBCardText>
+              {item.description.substring(0,100)}
+            </MDBCardText>
+            <MDBBtn style={{width:"130px",height:"36px"}}>View Details</MDBBtn>&nbsp;&nbsp;
+            <MDBBtn style={{width:"130px",height:"36px"}}>AddtoCart</MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>  
+        )
+       }
+      
+      
+      
+    </MDBRow>
+    </div>
+  )
+}
+export default Home
---
+++ src/Routing.js
@@ -0,0 +1,30 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Home';
+
+
+function Routing() {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path='/' Component={Home}></Route>
+      </Routes>
+
+      
+    </>
+  );
+}
+
+export default Routing;
